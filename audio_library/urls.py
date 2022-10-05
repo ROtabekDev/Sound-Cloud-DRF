b4110851f6a31cffb659f333cba34e109228f819
Otabek <otabekrahmonovtest@gmail.com>
2022-10-05 09:27:20 +0500

    CRUD Litsenziya

--- audio_library/urls.py
+++ audio_library/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('genre/', views.GenreListAPIView.as_view(), name='genres'),
+
+    path('license/', views.LicenseViewSet.as_view({'get': 'list', 'post': 'create'})), 
+    path('license/<int:pk>/', views.LicenseViewSet.as_view({'put': 'update', "delete": 'destroy'}))
 ]
--- audio_library/views.py
+++ audio_library/views.py
@@ -1,8 +1,23 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 
 from . import models
 from . import serializers
 
+from base.permissions import IsAuthor
+
 class GenreListAPIView(generics.ListAPIView):
     queryset = models.Genre.objects.all()
     serializer_class = serializers.GenreSerializer
+
+
+class LicenseViewSet(viewsets.ModelViewSet):
+    """CRUD
+    """
+    serializer_class = serializers.LicenseSerializer
+    permission_classes = [IsAuthor]
+
+    def get_queryset(self):
+        return models.License.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)

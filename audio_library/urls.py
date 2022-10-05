ed1bc7e7577f04922f7e89a09dec3c8855aba884
Otabek <otabekrahmonovtest@gmail.com>
2022-10-05 09:14:13 +0500

    Janrlar

--- audio_library/urls.py
+++ audio_library/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('genre/', views.GenreListAPIView.as_view(), name='genres'),
 ]
--- audio_library/views.py
+++ audio_library/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from . import models
+from . import serializers
+
+class GenreListAPIView(generics.ListAPIView):
+    queryset = models.Genre.objects.all()
+    serializer_class = serializers.GenreSerializer
